a542b20269d5e8f8439fba8ae8c0c79e1b7cff6f
Adam Hanna <ahanna@alumni.mines.edu>
2021-05-07 18:23:01 -0700

    finished poc

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,8 @@
-#[cfg(windows)]
-#[macro_use]
-extern crate memoffset;
-#[cfg(windows)]
-#[macro_use]
-extern crate winapi;
 extern crate vst;
 extern crate web_view;
 
 use std::error::Error;
 use std::os::raw::c_void;
-
-use web_view::*;
-
-#[cfg(windows)]
-mod win32;
 
 mod lib {
     use std::error::Error;
@@ -32,48 +21,56 @@ mod lib {
 }
 
 pub struct PluginGui {
-    gui: Box<dyn lib::PluginGui>,
+    js: JavascriptCallback,
+    html: String,
 }
 
 impl PluginGui {
     // Calls the Javascript 'eval' function with the specified argument.
     // This method always returns an error when the plugin window is closed.
-    pub fn execute(&self, javascript_code: &str) -> Result<(), Box<dyn Error>> {
-        self.gui.execute(javascript_code)
+    pub fn execute(&mut self, _javascript_code: &str) -> Result<(), Box<dyn Error>> {
+        // note: this is wrong...
+        // self.gui.eval(javascript_code);
+        Ok(())
     }
 }
 
 impl vst::editor::Editor for PluginGui {
     fn size(&self) -> (i32, i32) {
-        self.gui.size()
+        (1920, 2048)
     }
 
     fn position(&self) -> (i32, i32) {
-        self.gui.position()
+        (0,0)
     }
 
     fn close(&mut self) {
-        self.gui.close()
+        
     }
 
-    fn open(&mut self, parent_handle: *mut c_void) -> bool {
-        let html_content = "<html><body><h1>Hello, World!</h1></body></html>";
-        web_view::builder()
-            .title("This is a test")
-            .content(Content::Html(html_content))
-            .size(320, 480)
-            .resizable(false)
+    fn open(&mut self, _parent_handle: *mut c_void) -> bool {
+        let h = self.html.clone();
+        web_view::WebViewBuilder::new()
+            .title("VST")
+            .content(web_view::Content::Html(h))
+            .size(1920, 2048)
+            .resizable(true)
             .debug(true)
             .user_data(())
-            .invoke_handler(|_webview, _arg| Ok(()))
+            .invoke_handler(|_webview, arg| {
+                (self.js)(arg.to_string());
+                Ok(())
+            })
+            .build()
+            .unwrap()
             .run()
             .unwrap();
 
-        self.gui.open(parent_handle)
+        true
     }
 
     fn is_open(&mut self) -> bool {
-        self.gui.is_open()
+       true
     }
 }
 
@@ -82,12 +79,10 @@ pub use lib::JavascriptCallback;
 pub fn new_plugin_gui(
     html_document: String,
     js_callback: JavascriptCallback,
-    window_size: Option<(i32, i32)>) -> PluginGui
+    _window_size: Option<(i32, i32)>) -> PluginGui
 {
-    #[cfg(windows)]
-    {
-        PluginGui {
-            gui: win32::new_plugin_gui(html_document, js_callback, window_size)
-        }
+    PluginGui {
+        js: js_callback,
+        html: html_document,
     }
 }
--- src/win32/mod.rs
+++
@@ -1,8 +0,0 @@
-#[allow(dead_code, non_snake_case)]
-mod client_site;
-mod com_pointer;
-mod gui;
-#[allow(dead_code, non_snake_case, non_upper_case_globals)]
-mod ffi;
-
-pub use win32::gui::new_plugin_gui;
